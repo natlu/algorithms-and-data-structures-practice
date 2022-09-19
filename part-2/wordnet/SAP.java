1c4c7c1f73b7b2f62d0e0fbcf154240057453167
nathan.lu <natlu@users.noreply.github.com>
2022-09-19 22:33:37 +1000

    begin attempt at iterable solution

--- part-2/wordnet/SAP.java
+++ part-2/wordnet/SAP.java
@@ -10,7 +10,12 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class SAP {
 
@@ -38,6 +43,112 @@ public class SAP {
         }
         lengthCache.get(x).put(y, minLen);
         ancestorCache.get(x).put(y, ancestor);
+    }
+
+    private int getHashCode(Iterable<Integer> it) {
+        HashSet<Integer> hs = new HashSet<>((Collection) it);
+        ArrayList<Integer> al = new ArrayList<>();
+        al.addAll(hs);
+        Collections.sort(al);
+        return al.hashCode();
+    }
+
+    private Integer queryCache(HashMap<Integer, HashMap<Integer, Integer>> cache,
+                               Iterable<Integer> vs,
+                               Iterable<Integer> ws) {
+        int vhash = getHashCode(vs);
+        int whash = getHashCode(ws);
+
+        int x;
+        int y;
+        if (vhash <= whash) {
+            x = vhash;
+            y = whash;
+        }
+        else {
+            x = whash;
+            y = vhash;
+        }
+
+        if (!cache.containsKey(x)) {
+            cache.put(x, new HashMap<>());
+            return null;
+        }
+        return cache.get(x).get(y);
+    }
+
+    private void cache(Iterable<Integer> vs, Iterable<Integer> ws, int minLen, int ancestor) {
+        int vhash = getHashCode(vs);
+        int whash = getHashCode(ws);
+
+        int x;
+        int y;
+        if (vhash <= whash) {
+            x = vhash;
+            y = whash;
+        }
+        else {
+            x = whash;
+            y = vhash;
+        }
+
+        lengthCache.get(x).put(y, minLen);
+        ancestorCache.get(x).put(y, ancestor);
+    }
+
+    public void calc(Iterable<Integer> vs, Iterable<Integer> ws) {
+        Integer resLength = queryCache(lengthCache, vs, ws);
+        Integer resAncestor = queryCache(ancestorCache, vs, ws);
+        if (resLength != null && resAncestor != null) {
+            // return because already calculated this
+            return;
+        }
+
+        BreadthFirstDirectedPaths vg = new BreadthFirstDirectedPaths(G, vs);
+        BreadthFirstDirectedPaths wg = new BreadthFirstDirectedPaths(G, ws);
+
+        Queue<Integer> lst = new Queue<>();
+        // keep track of what has been put in the queue to avoid duplicates
+        boolean[] enqueued = new boolean[G.V()];
+
+        for (int v : vs) {
+            lst.enqueue(v);
+            enqueued[v] = true;
+        }
+        for (int w : ws) {
+            lst.enqueue(w);
+            enqueued[w] = true;
+        }
+
+
+        int len = Integer.MAX_VALUE;
+        int ancestor = -1;
+
+        while (!lst.isEmpty()) {
+            Integer i = lst.dequeue();
+            if (vg.hasPathTo(i) && wg.hasPathTo(i)) {
+                int vd = vg.distTo(i);
+                int wd = wg.distTo(i);
+                if (vd >= len || wd >= len) {
+                    break;
+                }
+                if ((vd + wd) < len) {
+                    len = vd + wd;
+                    ancestor = i;
+                }
+            }
+            for (int j : G.adj(i)) {
+                if (!enqueued[j]) {
+                    lst.enqueue(j);
+                    enqueued[j] = true;
+                }
+            }
+        }
+
+        if (len == Integer.MAX_VALUE) {
+            len = -1;
+        }
+        cache(vs, ws, len, ancestor);
     }
 
     private Integer getLengthCache(int v, int w) {
@@ -146,9 +257,12 @@ public class SAP {
         return getAncestorCache(v, w);
     }
 
-    // // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    // public int length(Iterable<Integer> v, Iterable<Integer> w)
-    //
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        calc(v, w);
+        return queryCache(ancestorCache, v, w);
+    }
+
     // // a common ancestor that participates in shortest ancestral path; -1 if no such path
     // public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
 
@@ -180,5 +294,41 @@ public class SAP {
         x = 1;
         y = 6;
         StdOut.printf("length = %d ancestor = %d\n", sap.length(x, y), sap.ancestor(x, y));
+
+
+        // https://docs.oracle.com/javase/6/docs/api/java/util/List.html#hashCode()
+        List<Integer> foo1 = new ArrayList<>();
+        foo1.add(1);
+        foo1.add(2);
+        foo1.add(3);
+
+        List<Integer> foo2 = new ArrayList<>();
+        foo2.add(1);
+        foo2.add(2);
+        foo2.add(3);
+
+        boolean iseq = foo1.hashCode() == foo2.hashCode();
+        System.out.println(iseq);
+
+        System.out.println(foo1.hashCode());
+
+        List<Integer> foo0 = new ArrayList<>();
+        foo0.add(2);
+        System.out.println(foo0.hashCode());
+
+
+        HashSet<Integer> hs = new HashSet<>();
+        hs.add(1);
+        hs.add(2);
+
+        HashSet<Integer> hs2 = new HashSet<>();
+        hs2.add(3);
+
+        boolean hshs = hs.hashCode() == hs.hashCode();
+        System.out.println(hshs);
+        System.out.println(hs.hashCode());
+        System.out.println(hs2.hashCode());
+
+
     }
 }
