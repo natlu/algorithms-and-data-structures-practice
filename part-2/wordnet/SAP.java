787cd51842bc72e83bf838b83badef1cb17fe598
nathan.lu <natlu@users.noreply.github.com>
2022-09-19 22:35:44 +1000

    make everything use iterable methods

--- part-2/wordnet/SAP.java
+++ part-2/wordnet/SAP.java
@@ -151,116 +151,26 @@ public class SAP {
         cache(vs, ws, len, ancestor);
     }
 
-    private Integer getLengthCache(int v, int w) {
-        int x;
-        int y;
-        if (v <= w) {
-            x = v;
-            y = w;
-        }
-        else {
-            x = w;
-            y = v;
-        }
-
-        if (!lengthCache.containsKey(x)) {
-            // System.out.println("length cache doesnt exist");
-            lengthCache.put(x, new HashMap<>());
-            return null;
-        }
-        // System.out.println("reading length cache");
-        return lengthCache.get(x).get(y);
-    }
-
-    private Integer getAncestorCache(int v, int w) {
-        int x;
-        int y;
-        if (v <= w) {
-            x = v;
-            y = w;
-        }
-        else {
-            x = w;
-            y = v;
-        }
-
-        if (!ancestorCache.containsKey(x)) {
-            // System.out.println("ancestor cache doesnt exist");
-            ancestorCache.put(x, new HashMap<>());
-            return null;
-        }
-        // System.out.println("reading ancestor cache");
-        return ancestorCache.get(x).get(y);
-    }
-
-    public void calc(int v, int w) {
-        Integer resLength = getLengthCache(v, w);
-        Integer resAncestor = getAncestorCache(v, w);
-        if (resLength != null && resAncestor != null) {
-            // return because already calculated this
-            return;
-        }
-
-        BreadthFirstDirectedPaths vg = new BreadthFirstDirectedPaths(G, v);
-        BreadthFirstDirectedPaths wg = new BreadthFirstDirectedPaths(G, w);
-
-        Queue<Integer> lst = new Queue<>();
-        lst.enqueue(v);
-        lst.enqueue(w);
-
-        // keep track of what has been put in the queue to avoid duplicates
-        boolean[] enqueued = new boolean[G.V()];
-        enqueued[v] = true;
-        enqueued[w] = true;
-
-        int len = Integer.MAX_VALUE;
-        int ancestor = -1;
-
-        while (!lst.isEmpty()) {
-            Integer i = lst.dequeue();
-            if (vg.hasPathTo(i) && wg.hasPathTo(i)) {
-                int vd = vg.distTo(i);
-                int wd = wg.distTo(i);
-                if (vd >= len || wd >= len) {
-                    break;
-                }
-                if ((vd + wd) < len) {
-                    len = vd + wd;
-                    ancestor = i;
-                }
-            }
-            for (int j : G.adj(i)) {
-                if (!enqueued[j]) {
-                    lst.enqueue(j);
-                    enqueued[j] = true;
-                }
-            }
-        }
-
-        if (len == Integer.MAX_VALUE) {
-            len = -1;
-        }
-        cache(v, w, len, ancestor);
-    }
-
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        // System.out.println("getting length cache");
-        calc(v, w);
-        return getLengthCache(v, w);
+        List<Integer> vs = new ArrayList<>(v);
+        List<Integer> ws = new ArrayList<>(w);
+        calc(vs, ws);
+        return queryCache(lengthCache, vs, ws);
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        // System.out.println("getting ancestor cache");
-        calc(v, w);
-        return getAncestorCache(v, w);
+        List<Integer> vs = new ArrayList<>(v);
+        List<Integer> ws = new ArrayList<>(w);
+        calc(vs, ws);
+        return queryCache(ancestorCache, vs, ws);
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
         calc(v, w);
-        return queryCache(ancestorCache, v, w);
+        return queryCache(lengthCache, v, w);
     }
 
     // // a common ancestor that participates in shortest ancestral path; -1 if no such path
