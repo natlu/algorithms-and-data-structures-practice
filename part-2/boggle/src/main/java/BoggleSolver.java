b132c779eedaad0f70f62b8e61fcdb93004f4308
nathan.lu <natlu@users.noreply.github.com>
2023-02-05 20:34:53 +1100

    fix array pos

--- part-2/boggle/src/main/java/BoggleSolver.java
+++ part-2/boggle/src/main/java/BoggleSolver.java
@@ -1,6 +1,7 @@
 import com.sun.jdi.Value;
 
 public class BoggleSolver {
+    Trie dictionaryTrie;
 
     private class Trie {
         static final int R = 26;
@@ -15,12 +16,12 @@ public class BoggleSolver {
             root = put(root, key, 0);
         }
 
-        private Node put(Node node, String key, int character_index)
-        {
+        private Node put(Node node, String key, int character_index) {
             if (node == null) node = new Node();
             if (character_index == key.length()) { node.wordEnd = true; return node; }
             char c = key.charAt(character_index);
-            node.next[c] = put(node.next[c], key, character_index+1);
+            int pos = c - 'A';
+            node.next[pos] = put(node.next[pos], key, character_index+1);
             return node;
         }
 
@@ -33,17 +34,45 @@ public class BoggleSolver {
             if (node.wordEnd) return true;
             if (character_index == key.length()) return false; // must be after checking wordEnd
             char c = key.charAt(character_index);
-            return contains(node.next[c], key, character_index+1);
+            int pos = c - 'A';
+            return contains(node.next[pos], key, character_index+1);
+        }
+    }
+
+    public BoggleSolver(String[] dictionary) {
+        dictionaryTrie = new Trie();
+        for (String word: dictionary) {
+            dictionaryTrie.put(word);
+        }
+    }
+
+    public boolean foo(String key) {
+        return dictionaryTrie.contains(key);
+    }
+
+    // public Iterable<String> getAllValidWords(BoggleBoard board)
+    // public int scoreOf(String word)
+    public static void main(String[] args) {
+
+//        Integer[] a = new Integer[26];
+//        for (int i=0; i < 26; i++) {
+//            a[i] = i;
+//        }
+//        System.out.println(a['a'-'a']);
+//        System.out.println(a['b'-'a']);
+//        System.out.println(a['A'-'A']);
+//        System.out.println(a['B'-'A']);
+
+        String[] dict = {"ONE","TWO", "THREE", "FOUR", "FIVE"};
+        BoggleSolver bs = new BoggleSolver(dict);
+
+        String[] tests = {"ONE", "FOUR", "EIGHT", "AAA"};
+        for (String test: tests) {
+            if (bs.foo(test)) {
+                System.out.println(test);
+            }
         }
 
 
     }
-
-    public BoggleSolver(String[] dictionary) {
-
-    }
-
-    }
-    // public Iterable<String> getAllValidWords(BoggleBoard board)
-    // public int scoreOf(String word)
 }
